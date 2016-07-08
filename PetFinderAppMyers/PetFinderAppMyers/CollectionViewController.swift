79fb0a718c660869057623c408916964432922fd
Chris Myers <cmyers78@gmail.com>
2016-07-07 22:25:48 -0600

    stubbed in collection view delegates and protocols

--- PetFinderAppMyers/PetFinderAppMyers/CollectionViewController.swift
+++ PetFinderAppMyers/PetFinderAppMyers/CollectionViewController.swift
@@ -8,16 +8,39 @@
 
 import UIKit
 
-class CollectionViewController: UIViewController {
+class CollectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    var currentDog = [Dog]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
+        for dog in 0...19 {
+            
+            if let d = DataStorage.sharedInstance.dogAtIndex(dog) {
+                self.currentDog.append(d)
+                
+                print(self.currentDog.count)
+            }
+        }
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 1
     }
 
     
-    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionCell", forIndexPath: indexPath)
+        
+        
+        
+        return cell
+    }
 
 
 }
