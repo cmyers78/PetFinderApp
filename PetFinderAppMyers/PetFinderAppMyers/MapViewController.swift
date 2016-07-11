f360e7a638374572b75aa3e0f2932d333078bb96
Chris Myers <cmyers78@gmail.com>
2016-07-11 12:02:06 -0600

    added map features and 1st pin

--- PetFinderAppMyers/PetFinderAppMyers/MapViewController.swift
+++ PetFinderAppMyers/PetFinderAppMyers/MapViewController.swift
@@ -7,31 +7,47 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Locations"
-
+        
+        addPin(40.730459, pinLong: -111.551152, pinTitle: "The Adoption Center - Kimball Junction", pinSubtitle: "6699 N Landmark Dr, Suite B-103C, Park City, UT 84098")
+        
+        
         // Do any additional setup after loading the view.
     }
+    
+    func addPin(pinLat : Double, pinLong : Double, pinTitle : String, pinSubtitle : String) {
+        let location = CLLocationCoordinate2D(latitude: pinLat, longitude: pinLong)
+        
+        let annotation = MKPointAnnotation()
+        
+        annotation.coordinate = location
+        annotation.title = pinTitle
+        annotation.subtitle = pinSubtitle
+        
+        self.mapView.addAnnotation(annotation)
+        
+        self.centerMap(location)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func centerMap(centerCoord : CLLocationCoordinate2D) {
+        let span = MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        
+        let region = MKCoordinateRegion(center: centerCoord, span: span)
+     
+        
+        self.mapView.setRegion(region, animated: true)
     }
-    */
 
+    
 }
